d7395f5bf3fce08845da37f2ecf4ce54f9998ca7
quangvuong2541 <truongquangvuong2000@gmail.com>
2023-09-23 21:34:55 +0700

    tookit

--- demo.js
+++ demo.js
@@ -1,7 +1,10 @@
 const redux = require('redux')
 const createStore = redux.createStore
-
+const bindActionCreators = redux.bindActionCreators
 const FETCH_API = "FETCH_API"
+const REST = "REST"
+const FETCH_API_1 = "FETCH_API_1"
+const REST_1 = "REST_1"
 
 function fetchAPI() {
     return {
@@ -9,9 +12,33 @@ function fetchAPI() {
         soLuong: 20
     }
 }
+
+function restockToys(sl = 5) {
+    return {
+        type: REST,
+        soLuong: sl
+
+    }
+}
+
+function fetchAPI_1(qty = 1) {
+    return {
+        type: FETCH_API_1,
+        payload: qty
+    }
+}
+
+
+function restockToys_1(qty = 1) {
+    return {
+        type: REST_1,
+        payload: qty
+    }
+}
 //  reducer
 const initalState = {
     number: 10,
+    number1: 20
 }
 const reducer = (state = initalState, action) => {
     switch (action.type) {
@@ -19,6 +46,21 @@ const reducer = (state = initalState, action) => {
             return {
                 ...state,
                 number: state.number - 1
+            }
+        case REST:
+            return {
+                ...state,
+                number: state.number + action.soLuong
+            }
+        case FETCH_API_1:
+            return {
+                ...state,
+                number1: state.number1 - 1
+            }
+        case REST_1:
+            return {
+                ...state,
+                number1: state.number1 + action.payload
             }
         default:
             return state
@@ -31,8 +73,18 @@ console.log("king", store.getState());
 
 const demo = store.subscribe(() => console.log('update state', store.getState()))
 
-store.dispatch(fetchAPI())
-store.dispatch(fetchAPI())
-store.dispatch(fetchAPI())
+// store.dispatch(fetchAPI())
+// store.dispatch(fetchAPI())
+// store.dispatch(fetchAPI())
+// store.dispatch(restockToys(3))
+
+
+const actions = bindActionCreators({ fetchAPI, restockToys, fetchAPI_1, restockToys_1 }, store.dispatch)
+actions.fetchAPI()
+actions.fetchAPI()
+actions.fetchAPI()
+actions.restockToys(3)
+actions.fetchAPI_1()
+actions.restockToys_1(2)
 demo()
 
